68f5511ec1d54237eb1040653557a1a62494479e
Keep-Foolish <1571567297@qq.com>
2024-01-14 17:56:27 +0800

    vue-router

--- Vue/learn_vue/src/components/HelloWorld.vue
+++ Vue/learn_vue/src/components/HelloWorld.vue
@@ -1,23 +1,13 @@
 <template>
-  <div class="container" v-show="false">
-  </div>
-  <div class="containera">
+  <div>
+    
   </div>
 </template>
 
 <script setup>
-  const a = false
+  
 </script>
 
-<style lang='css' scoped>
-  .container{
-    height: 100px;
-    width: 100px;
-    background-color: aqua;
-  }
-  .containera{
-    height: 100px;
-    width: 100px;
-    background-color: aqua;
-  }
+<style lang='scss' scoped>
+  
 </style>
